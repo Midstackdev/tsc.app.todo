9f13e61030179f468ef5ab634eb49a3daa8e51f6
Midstackdev <midstackdev@gmail.com>
2023-03-03 10:22:41 +0000

    connect db

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,40 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import cors from 'cors';
+
+dotenv.config();
+const port = process.env.PORT;
+// console.log('---ENV---', process.env);
 
 const app: Express = express();
 
-const port = 5000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express Typescript");
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_DB_HOST,
+  port: Number(process.env.MYSQL_DB_PORT),
+  username: process.env.MYSQL_DB_USER,
+  password: process.env.MYSQL_DB_PASSWORD,
+  database: process.env.MYSQL_DB_DATABASE,
+  synchronize: true,
 });
 
-app.listen(port);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express Typescript');
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log('Data Source has been initialized');
+  })
+  .catch((err) => {
+    console.log({
+      message: 'Erorr during Data Source initialization',
+      err,
+    });
+  });

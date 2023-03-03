0a58fb3dc26599d93d7fa635ba7a7ad81288bac9
Midstackdev <midstackdev@gmail.com>
2023-03-03 08:11:08 +0000

    start server

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+const port = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express Typescript");
+});
+
+app.listen(port);

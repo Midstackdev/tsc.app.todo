d3c68ab1949d83ac11e7594ad5a828f98769c110
Midstackdev <midstackdev@gmail.com>
2023-03-09 00:55:14 +0000

    react query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { Dashboard } from './pages/Dashboard';
 import { customTheme } from './theme/customTheme';
 
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
--- client/src/components/createTaskForm/CreateTaskForm.tsx
+++ client/src/components/createTaskForm/CreateTaskForm.tsx
@@ -1,5 +1,13 @@
-import { Box, Stack, Typography } from '@mui/material';
-import React, { FC, ReactElement } from 'react';
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  LinearProgress,
+  Stack,
+  Typography,
+} from '@mui/material';
+import React, { FC, ReactElement, useState } from 'react';
 import { Priority } from './enums/Priority';
 import { Status } from './enums/Status';
 import { DateField } from './_dateField';
@@ -8,6 +16,11 @@ import { SelectField } from './_selectField';
 import { TitleField } from './_titleField';
 
 export const CreateTaskForm: FC = (): ReactElement => {
+  const [title, setTitle] = useState<string | undefined>(undefined);
+  const [description, setDescription] = useState<string | undefined>(undefined);
+  const [date, setDate] = useState<Date | null>(new Date());
+  const [status, setStatus] = useState<string>(Status.todo);
+  const [priority, setPriority] = useState<string>(Priority.normal);
   const statusItems = Object.keys(Status)
     .map((x) => {
       return {
@@ -31,18 +44,44 @@ export const CreateTaskForm: FC = (): ReactElement => {
       px={4}
       my={6}
     >
+      <Alert
+        severity="success"
+        sx={{
+          width: '100%',
+          marginBottom: '16px',
+        }}
+      >
+        <AlertTitle>Success</AlertTitle>
+        The task has been created successfully
+      </Alert>
       <Typography mb={2} component="h2" variant="h6">
         Create A Task
       </Typography>
       <Stack width={'100%'} spacing={2}>
-        <TitleField />
-        <DescriptionField />
-        <DateField />
+        <TitleField onChange={(e) => setTitle(e.target.value)} />
+        <DescriptionField onChange={(e) => setDescription(e.target.value)} />
+        <DateField value={date} onChange={(date) => setDate(date)} />
         <Stack direction={'row'} spacing={2} sx={{ width: '100%' }}>
-          <SelectField name="status" label="Status" items={statusItems} />
+          <SelectField
+            name="status"
+            label="Status"
+            items={statusItems}
+            value={status}
+            onChange={(e) => setStatus(e.target.value as string)}
+          />
 
-          <SelectField name="priority" label="Priority" items={priorityItems} />
+          <SelectField
+            name="priority"
+            label="Priority"
+            items={priorityItems}
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as string)}
+          />
         </Stack>
+        <LinearProgress />
+        <Button variant="contained" size="large" fullWidth>
+          Create A Task
+        </Button>
       </Stack>
     </Box>
   );
